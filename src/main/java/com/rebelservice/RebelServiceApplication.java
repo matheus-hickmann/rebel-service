75da56a20f251eee2d899137dfc35f088a6ac969
Matheus Hickmann <matheus.hickmann@aegro.com.br>
2021-11-20 09:57:48 -0300

    feat: Add spring-boot with dependencies

---
+++ src/main/java/com/rebelservice/RebelServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rebelservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebelServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebelServiceApplication.class, args);
+	}
+
+}
